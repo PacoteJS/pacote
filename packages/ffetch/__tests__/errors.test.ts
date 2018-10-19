5a0d90cf94061e92b1caecceee7c443c0f5dfe54
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-10-19 12:49:51 +0100

    Refactor tests

--- packages/ffetch/__tests__/errors.test.ts
+++ packages/ffetch/__tests__/errors.test.ts
@@ -1,42 +1,50 @@
 import { StatusError, ParserError, NetworkError } from '../src/errors'
 
 describe('StatusError', () => {
-  it('is instance of Error', () => {
-    const error = new StatusError(500, 'Internal server error')
+  const status = 500
+
+  it('is an instance of Error', () => {
+    const error = new StatusError(status)
     expect(error).toBeInstanceOf(Error)
   })
 
-  it('is instance of StatusError', () => {
-    const error = new StatusError(500, 'Internal server error')
+  it('is an instance of StatusError', () => {
+    const error = new StatusError(status)
     expect(error).toBeInstanceOf(StatusError)
   })
 
   it('records a stack', () => {
-    const error = new StatusError(500, 'Internal server error')
+    const error = new StatusError(status)
     expect(error.stack).toBeTruthy()
   })
 
-  it('holds the response status code', () => {
-    const error = new StatusError(500, 'Internal server error')
-    expect(error.status).toBe(500)
+  it('has the response status code', () => {
+    const error = new StatusError(status)
+    expect(error.status).toBe(status)
   })
 
-  it('holds the response body', () => {
+  it('has a message', () => {
+    const message = 'Internal server error'
+    const error = new StatusError(status, message)
+    expect(error.message).toBe(message)
+  })
+
+  it('has a response body', () => {
     const body = {
       content: 'parsed'
     }
-    const error = new StatusError<{}>(500, 'Internal server error', body)
+    const error = new StatusError<{}>(status, '', body)
     expect(error.body).toEqual(body)
   })
 })
 
 describe('NetworkError', () => {
-  it('is instance of Error', () => {
+  it('is an instance of Error', () => {
     const error = new NetworkError()
     expect(error).toBeInstanceOf(Error)
   })
 
-  it('is instance of NetworkError', () => {
+  it('is an instance of NetworkError', () => {
     const error = new NetworkError()
     expect(error).toBeInstanceOf(NetworkError)
   })
@@ -46,26 +54,26 @@ describe('NetworkError', () => {
     expect(error.stack).toBeTruthy()
   })
 
-  it('holds the response status code', () => {
+  it('has a message', () => {
     const message = 'test error'
     const error = new NetworkError(message)
     expect(error.message).toBe(message)
   })
 
-  it('holds any causing errors', () => {
-    const cause = new Error('cause error')
-    const error = new NetworkError('test error', [cause])
+  it('references originating errors', () => {
+    const cause = new Error('')
+    const error = new NetworkError('', [cause])
     expect(error.causes).toEqual([cause])
   })
 })
 
 describe('ParserError', () => {
-  it('is instance of Error', () => {
+  it('is an instance of Error', () => {
     const error = new ParserError()
     expect(error).toBeInstanceOf(Error)
   })
 
-  it('is instance of ParserError', () => {
+  it('is an instance of ParserError', () => {
     const error = new ParserError()
     expect(error).toBeInstanceOf(ParserError)
   })
@@ -75,15 +83,15 @@ describe('ParserError', () => {
     expect(error.stack).toBeTruthy()
   })
 
-  it('holds the response status code', () => {
+  it('has a message', () => {
     const message = 'test error'
-    const error = new ParserError(message)
+    const error = new NetworkError(message)
     expect(error.message).toBe(message)
   })
 
-  it('holds any causing errors', () => {
-    const cause = new Error('cause error')
-    const error = new ParserError('test error', [cause])
+  it('references originating errors', () => {
+    const cause = new Error('')
+    const error = new ParserError('', [cause])
     expect(error.causes).toEqual([cause])
   })
 })
