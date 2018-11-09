183a1aeb9ecb81a10f174c68fa65c994b7244577
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-11-09 14:30:19 +0000

    Refactor test

--- packages/flux-actions/test/complex.spec.ts
+++ packages/flux-actions/test/complex.spec.ts
@@ -1,33 +1,31 @@
 import { createAction, reducerFromState } from '../src/index'
 import { Either, left, right } from 'fp-ts/lib/Either'
 
-type State = {
-  year: number
-  error: Error | null
-}
-
 test('complex paylaods', () => {
-  const changeYear = createAction<Either<Error, number>>('CHANGE_YEAR')
+  type Payload = Either<Error, number>
 
-  const reducer = reducerFromState<State>({ year: 1985, error: null })
-    // CHANGE_YEAR
-    .on(changeYear, (state, { payload }) =>
-      payload.fold<State>(
-        error => ({ ...state, error }),
+  type State = {
+    year: number
+    error: Error | null
+  }
+
+  const changeYear = createAction<Payload>('CHANGE_YEAR')
+
+  const reducer = reducerFromState<State>({ year: 1985, error: null }).on(
+    changeYear,
+    (s, a) =>
+      a.payload.fold<State>(
+        error => ({ ...s, error }),
         year => ({ year, error: null })
       )
-    )
+  ).run
 
-  expect(
-    reducer.run(undefined, changeYear(right<Error, number>(1955)))
-  ).toEqual({
+  expect(reducer(undefined, changeYear(right<Error, number>(1955)))).toEqual({
     year: 1955,
     error: null
   })
 
-  expect(
-    reducer.run(undefined, changeYear(left<Error, number>(Error())))
-  ).toEqual({
+  expect(reducer(undefined, changeYear(left<Error, number>(Error())))).toEqual({
     year: 1985,
     error: Error()
   })
