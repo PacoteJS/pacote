9fedf195642a26d27565aa788f0feb05deb7d17b
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-11-20 17:13:01 +0000

    TO DO

--- packages/flux-actions/test/index.spec.ts
+++ packages/flux-actions/test/index.spec.ts
@@ -94,13 +94,22 @@ test('reducer matches a list of actions', () => {
 })
 
 test('reducerFromState() returns a legacy .run() method', () => {
-  const whatever = createAction('@@TEST/WHATEVER')
+  const action = createAction('@@TEST/ACTION')
   const reducer = reducerFromState('')
-  expect(reducer.run('unchanged', whatever())).toBe('unchanged')
+  expect(reducer.run('unchanged', action())).toBe('unchanged')
 })
 
 test('reducerFromState().on() returns a legacy .run() method', () => {
   const name = createAction<string>('@@TEST/NAME')
   const reducer = reducerFromState('').on(name, (s, a) => a.payload)
   expect(reducer.run('', name('Dr Emmet L. Brown'))).toBe('Dr Emmet L. Brown')
+})
+
+test.skip('reducer is immutable', () => {
+  const action = createAction('@@TEST/ACTION')
+  const reducer = reducerFromState('').on(action, () => 'immutability ok')
+
+  reducer.on(action, () => 'immutability nok')
+
+  expect(reducer(undefined, action())).toBe('immutability ok')
 })
