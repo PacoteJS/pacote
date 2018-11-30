b7ca1d91275c29c90cdb93a7f1205de72f8f5569
Luis Rodrigues <goblindegook@users.noreply.github.com>
2018-11-30 02:03:59 +0000

    Tweaks

--- packages/react-with-props/src/index.ts
+++ packages/react-with-props/src/index.ts
@@ -9,15 +9,11 @@ import {
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
-type InnerDOMComponent = keyof ReactHTML | keyof ReactSVG
-
-type InnerComponent<P, T> = T extends InnerDOMComponent
-  ? ComponentType<P>
-  : InnerDOMComponent
+type InnerComponent<P> = ComponentType<P> | keyof ReactHTML | keyof ReactSVG
 
 type Enhanced<P extends I, I> = Omit<P & { children?: ReactNode }, keyof I>
 
-function getDisplayName(Component: InnerComponent<any, any>): string {
+function getDisplayName(Component: InnerComponent<any>): string {
   return typeof Component === 'string'
     ? Component
     : Component.displayName || Component.name || 'Component'
@@ -37,11 +33,11 @@ function getDisplayName(Component: InnerComponent<any, any>): string {
 
 export function withProps<I, P extends I, E = Enhanced<P, I>>(
   injectedProps: I,
-  BaseComponent: InnerComponent<P, any>
+  BaseComponent: InnerComponent<P>
 ): ComponentType<E> {
   const factory = createFactory(BaseComponent as FunctionComponent<P>)
-  const WrappedComponent: ComponentType<E> = props =>
+  const EnhancedComponent: ComponentType<E> = props =>
     factory(Object.assign<P, E, I>({} as any, props, injectedProps))
-  WrappedComponent.displayName = `WithProps(${getDisplayName(BaseComponent)})`
-  return WrappedComponent
+  EnhancedComponent.displayName = `WithProps(${getDisplayName(BaseComponent)})`
+  return EnhancedComponent
 }
