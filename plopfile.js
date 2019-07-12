1b74bfe62b9270ef28a8eb32af902de3c3bd6fcf
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-07-13 00:41:52 +0100

    React generator

--- plopfile.js
+++ plopfile.js
@@ -7,8 +7,8 @@ const data = {
 }
 
 module.exports = plop => {
-  plop.setGenerator('package', {
-    description: 'Basic Pacote package.',
+  plop.setGenerator('basic', {
+    description: 'Basic package.',
     prompts: [
       {
         type: 'input',
@@ -44,13 +44,63 @@ module.exports = plop => {
       {
         type: 'add',
         path: 'packages/{{ name }}/LICENSE',
-        templateFile: 'templates/basic/LICENSE.hbs',
+        templateFile: 'templates/LICENSE.hbs',
         data
       },
       {
         type: 'addMany',
         destination: 'packages/{{ name }}',
         base: 'templates/basic',
+        templateFiles: '**/*.*.hbs',
+        data
+      }
+    ]
+  })
+
+  plop.setGenerator('react', {
+    description: 'React package.',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Package name:',
+        validate: value => (/.+/.test(value) ? true : 'Required.')
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Package description:'
+      },
+      {
+        type: 'input',
+        name: 'authorName',
+        message: 'Author name:',
+        default: pkg.author.name
+      },
+      {
+        type: 'input',
+        name: 'authorEmail',
+        message: 'Author email:',
+        default: pkg.author.email
+      },
+      {
+        type: 'input',
+        name: 'authorUrl',
+        message: 'Author URL:',
+        default: pkg.author.url
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'packages/{{ name }}/LICENSE',
+        templateFile: 'templates/LICENSE.hbs',
+        data
+      },
+      {
+        type: 'addMany',
+        destination: 'packages/{{ name }}',
+        base: 'templates/react',
         templateFiles: '**/*.*.hbs',
         data
       }
