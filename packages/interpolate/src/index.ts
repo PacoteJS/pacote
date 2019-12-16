5c6178898aa648fcc0caf96593a363335f64aa0e
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-12-16 09:53:05 +0000

    fix: allows null types as interpolation values

--- packages/interpolate/src/index.ts
+++ packages/interpolate/src/index.ts
@@ -6,7 +6,7 @@ export function interpolate(
 ) {
   const regexp = RegExp(pattern, 'g')
 
-  return (data: ReplaceMap<string | number> = {}): string =>
+  return (data: ReplaceMap<string | number | null | undefined> = {}): string =>
     template.replace(regexp, (match, capture = '') => {
       const key = capture.trim()
       const value = Array.isArray(data) ? data[parseInt(key, 10)] : data[key]
