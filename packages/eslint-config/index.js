a7ba4365a732cb251d5738c47577ba00e591bf7a
Luis Rodrigues <goblindegook@users.noreply.github.com>
2020-05-21 20:31:59 +0100

    feat: 🎸 upgrade @typescript-eslint/eslint-plugin to 3.x
    BREAKING CHANGE: 🧨 The `@typescript-eslint/eslint-plugin` peer dependency is upgraded to a
    major version.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -18,6 +18,8 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // https://github.com/typescript-eslint/typescript-eslint/issues/1845
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'import/export': 'off',
     'no-array-constructor': 'off',
