495b30bd08e52c9aaf9da404451f9377db415d41
Luis Rodrigues <luis.rodrigues@springernature.com>
2020-08-07 01:45:24 +0100

    refactor: 💡 move internal methods to separate module

--- packages/linked-list/src/index.ts
+++ packages/linked-list/src/index.ts
@@ -1,51 +1,19 @@
-type Empty = undefined
-type Cons<T> = readonly [T, Cons<T> | Empty]
-type LinkedList<T> = Cons<T> | Empty
+import {
+  car,
+  cdr,
+  Cons,
+  empty,
+  isEmpty,
+  iterator,
+  LinkedList,
+  MapFn,
+  PredicateFn,
+  recursiveFind,
+  recursiveReduce,
+  ReduceFn,
+} from './internal'
 
-type MapFn<T, R> = (value: T, index: number, collection: LinkedList<T>) => R
-type PredicateFn<T> = MapFn<T, boolean>
-type ReduceFn<T, R> = (
-  acc: R,
-  value: T,
-  index: number,
-  collection: LinkedList<T>
-) => R
-
-function car<T>(cons: Cons<T>): T {
-  return cons[0]
-}
-
-function cdr<T>(cons: Cons<T>): Cons<T> | Empty {
-  return cons[1]
-}
-
-function empty<T>(): LinkedList<T> {
-  return undefined
-}
-
-export function isEmpty(list: any): list is Empty {
-  return list === undefined
-}
-
-function _reduce<T, R>(
-  acc: R,
-  reducer: ReduceFn<T, R>,
-  current: LinkedList<T>,
-  step: number,
-  index: number,
-  collection: LinkedList<T>
-): R {
-  return isEmpty(current)
-    ? acc
-    : _reduce(
-        reducer(acc, car(current), index, collection),
-        reducer,
-        cdr(current),
-        step,
-        index + step,
-        collection
-      )
-}
+export { isEmpty } from './internal'
 
 export function listOf<T>(...items: T[]): LinkedList<T> {
   return items
@@ -59,6 +27,10 @@ export function length<T>(list: LinkedList<T>): number {
 
 export function head<T>(list: LinkedList<T>): T | undefined {
   return isEmpty(list) ? undefined : car(list)
+}
+
+export function rest<T>(list: LinkedList<T>): LinkedList<T> {
+  return isEmpty(list) ? list : cdr(list)
 }
 
 export function reverse<T>(list: LinkedList<T>): LinkedList<T> {
@@ -75,10 +47,6 @@ export function prepend<T>(value: T, list: LinkedList<T>): Cons<T> {
 
 export function append<T>(value: T, list: LinkedList<T>): LinkedList<T> {
   return reverse(prepend(value, reverse(list)))
-}
-
-export function rest<T>(list: LinkedList<T>): LinkedList<T> {
-  return isEmpty(list) ? list : cdr(list)
 }
 
 export function concat<T>(
@@ -104,7 +72,7 @@ export function reduce<T, R>(
   initial: R,
   list: LinkedList<T>
 ): R {
-  return _reduce(initial, reducer, list, 1, 0, list)
+  return recursiveReduce(initial, reducer, list, 1, 0, list)
 }
 
 export function reduceRight<T, R>(
@@ -113,7 +81,7 @@ export function reduceRight<T, R>(
   list: LinkedList<T>
 ): R {
   const lastIndex = length(list) - 1
-  return _reduce(initial, reducer, reverse(list), -1, lastIndex, list)
+  return recursiveReduce(initial, reducer, reverse(list), -1, lastIndex, list)
 }
 
 export function map<T, R>(
@@ -144,22 +112,8 @@ export function filter<T>(
   )
 }
 
-function _find<T, R>(
-  predicate: (value: T, index: number) => boolean,
-  whenFound: (current: T, index: number) => R,
-  whenFinished: () => R,
-  current: LinkedList<T>,
-  index: number
-): R {
-  return isEmpty(current)
-    ? whenFinished()
-    : predicate(car(current), index)
-    ? whenFound(car(current), index)
-    : _find(predicate, whenFound, whenFinished, rest(current), index + 1)
-}
-
 export function indexOf<T>(value: T, list: LinkedList<T>): number {
-  return _find(
+  return recursiveFind(
     (current) => current === value,
     (_, index) => index,
     () => -1,
@@ -169,7 +123,7 @@ export function indexOf<T>(value: T, list: LinkedList<T>): number {
 }
 
 export function lastIndexOf<T>(value: T, list: LinkedList<T>): number {
-  const lastIndex = _find(
+  const lastIndex = recursiveFind(
     (current) => current === value,
     (_, index) => index,
     () => -1,
@@ -183,7 +137,7 @@ export function find<T>(
   predicate: PredicateFn<T>,
   list: LinkedList<T>
 ): T | undefined {
-  return _find(
+  return recursiveFind(
     (current, index) => predicate(current, index, list),
     (value) => value,
     () => undefined,
@@ -196,7 +150,7 @@ export function every<T>(
   predicate: PredicateFn<T>,
   list: LinkedList<T>
 ): boolean {
-  return _find(
+  return recursiveFind(
     (current, index) => !predicate(current, index, list),
     () => false,
     () => true,
@@ -209,7 +163,7 @@ export function some<T>(
   predicate: PredicateFn<T>,
   list: LinkedList<T>
 ): boolean {
-  return _find(
+  return recursiveFind(
     (current, index) => predicate(current, index, list),
     () => true,
     () => false,
@@ -218,47 +172,31 @@ export function some<T>(
   )
 }
 
-function _iterator<T, R>(
-  list: LinkedList<T>,
-  result: (key: number, value: T) => R
-): IterableIterator<R> {
-  let key = 0
-  let current = list
-
-  return {
-    [Symbol.iterator]: function () {
-      return this
-    },
-    next: function () {
-      if (isEmpty(current)) {
-        return { done: true, value: undefined }
-      } else {
-        const value = result(key++, car(current))
-        current = rest(current)
-        return { done: false, value }
-      }
-    },
-  }
+export function includes<T>(value: T, list: LinkedList<T>): boolean {
+  return some((v) => v === value, list)
 }
 
 export function entries<T>(list: LinkedList<T>): IterableIterator<[number, T]> {
-  return _iterator(list, (key, value) => [key, value])
+  return iterator(list, (key, value) => [key, value])
 }
 
 export function keys<T>(list: LinkedList<T>): IterableIterator<number> {
-  return _iterator(list, (key) => key)
+  return iterator(list, (key) => key)
 }
 
 export function values<T>(list: LinkedList<T>): IterableIterator<T> {
-  return _iterator(list, (_, value) => value)
+  return iterator(list, (_, value) => value)
 }
 
 export function get<T>(index: number, list: LinkedList<T>): T | undefined {
   return find((_, idx) => idx === index, list)
 }
 
-function _sliceFrom<T>(start: number, list: LinkedList<T>): LinkedList<T> {
-  return start > 0 ? _sliceFrom(start - 1, rest(list)) : list
+function recursiveSliceFrom<T>(
+  start: number,
+  list: LinkedList<T>
+): LinkedList<T> {
+  return start > 0 ? recursiveSliceFrom(start - 1, rest(list)) : list
 }
 
 export function slice<T>(
@@ -267,12 +205,8 @@ export function slice<T>(
   list: LinkedList<T>
 ): LinkedList<T> {
   const size = length(list)
-  const slicedList = _sliceFrom(start, list)
+  const slicedList = recursiveSliceFrom(start, list)
   return size > end
-    ? reverse(_sliceFrom(size - end, reverse(slicedList)))
+    ? reverse(recursiveSliceFrom(size - end, reverse(slicedList)))
     : slicedList
-}
-
-export function includes<T>(value: T, list: LinkedList<T>): boolean {
-  return some((v) => v === value, list)
 }
---
+++ packages/linked-list/src/internal.ts
@@ -0,0 +1,89 @@
+export type Empty = undefined
+export type Cons<T> = readonly [T, Cons<T> | Empty]
+export type LinkedList<T> = Cons<T> | Empty
+
+export type MapFn<T, R> = (
+  value: T,
+  index: number,
+  collection: LinkedList<T>
+) => R
+export type PredicateFn<T> = MapFn<T, boolean>
+export type ReduceFn<T, R> = (
+  acc: R,
+  value: T,
+  index: number,
+  collection: LinkedList<T>
+) => R
+
+export function car<T>(cons: Cons<T>): T {
+  return cons[0]
+}
+
+export function cdr<T>(cons: Cons<T>): LinkedList<T> {
+  return cons[1]
+}
+
+export function empty<T>(): LinkedList<T> {
+  return undefined
+}
+
+export function isEmpty(list: any): list is Empty {
+  return list === undefined
+}
+
+export function recursiveReduce<T, R>(
+  acc: R,
+  reducer: ReduceFn<T, R>,
+  current: LinkedList<T>,
+  step: number,
+  index: number,
+  collection: LinkedList<T>
+): R {
+  return isEmpty(current)
+    ? acc
+    : recursiveReduce(
+        reducer(acc, car(current), index, collection),
+        reducer,
+        cdr(current),
+        step,
+        index + step,
+        collection
+      )
+}
+
+export function recursiveFind<T, R>(
+  predicate: (value: T, index: number) => boolean,
+  whenFound: (current: T, index: number) => R,
+  whenFinished: () => R,
+  current: LinkedList<T>,
+  index: number
+): R {
+  return isEmpty(current)
+    ? whenFinished()
+    : predicate(car(current), index)
+    ? whenFound(car(current), index)
+    : recursiveFind(predicate, whenFound, whenFinished, cdr(current), index + 1)
+}
+
+export function iterator<T, R>(
+  list: LinkedList<T>,
+  result: (key: number, value: T) => R
+): IterableIterator<R> {
+  let key = 0
+  let current = list
+
+  return {
+    [Symbol.iterator]: function () {
+      return this
+    },
+    next: function () {
+      if (isEmpty(current)) {
+        return { done: true, value: undefined }
+      } else {
+        const value = result(key++, car(current))
+        current = cdr(current)
+        return { done: false, value }
+      }
+    },
+  }
+}
