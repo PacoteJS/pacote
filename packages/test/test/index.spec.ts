93922f49b260dc8e64653233e049c93f1c532302
Luis Rodrigues <luis.rodrigues@springernature.com>
2020-08-14 16:32:46 +0200

    test: 💍 execute tests without Jest

--- packages/test/test/index.spec.ts
+++ packages/test/test/index.spec.ts
@@ -1,58 +1,63 @@
 import assert from 'assert'
-import { test } from '../src/index'
+import { test } from '../src/index.js'
 
-it('reports successful test suites', async () => {
-  const t = test('named test suite')
+const testNamed = test('named test suite')
 
-  t('this is a basic test', () => {
-    assert.ok(true)
-  })
+testNamed('this is a basic test', () => {
+  assert.ok(true)
+})
 
-  const report = await t.run()
+const reportNamed = await testNamed.run()
 
-  assert.deepStrictEqual(report, {
+assert.deepStrictEqual(
+  reportNamed,
+  {
     name: 'named test suite',
     tests: [['this is a basic test', 'ok']],
-  })
+  },
+  'handles named tests'
+)
+
+const testSkipped = test('suite with skipped tests')
+
+testSkipped.skip('this is a skipped test', () => {
+  assert.ok(true)
 })
 
-it('reports suites with skipped tests', async () => {
-  const t = test('suite with skipped tests')
+const reportSkipped = await testSkipped.run()
 
-  t.skip('this is a skipped test', () => {
-    assert.ok(true)
-  })
-
-  const report = await t.run()
-
-  assert.deepStrictEqual(report, {
+assert.deepStrictEqual(
+  reportSkipped,
+  {
     name: 'suite with skipped tests',
     tests: [['this is a skipped test', 'skipped']],
-  })
+  },
+  'handles skipped tests'
+)
+
+const testOnly = test('suite with only tests')
+
+testOnly('this test will be skipped', () => {
+  assert.ok(true)
 })
 
-it('reports suites with only tests', async () => {
-  const t = test('suite with only tests')
+testOnly.only('only test', () => {
+  assert.ok(true)
+})
 
-  t('this test will be skipped', () => {
-    assert.ok(true)
-  })
+testOnly.only('another only test', () => {
+  assert.ok(true)
+})
 
-  t.only('only test', () => {
-    assert.ok(true)
-  })
+testOnly('this test will be skipped too', () => {
+  assert.ok(true)
+})
 
-  t.only('another only test', () => {
-    assert.ok(true)
-  })
+const reportOnly = await testOnly.run()
 
-  t('this test will be skipped too', () => {
-    assert.ok(true)
-  })
-
-  const report = await t.run()
-
-  assert.deepStrictEqual(report, {
+assert.deepStrictEqual(
+  reportOnly,
+  {
     name: 'suite with only tests',
     tests: [
       ['this test will be skipped', 'skipped'],
@@ -60,35 +65,40 @@ it('reports suites with only tests', async () => {
       ['another only test', 'ok'],
       ['this test will be skipped too', 'skipped'],
     ],
-  })
+  },
+  'handles only tests'
+)
+
+const testFailed = test('suite with failed tests')
+
+testFailed('this test will fail', () => {
+  assert.fail()
 })
 
-it('reports suites with failed tests', async () => {
-  const t = test('suite with failed tests')
+const reportFailed = await testFailed.run()
 
-  t('this test will fail', () => {
-    assert.fail()
-  })
-
-  const report = await t.run()
-
-  assert.deepStrictEqual(report, {
+assert.deepStrictEqual(
+  reportFailed,
+  {
     name: 'suite with failed tests',
     tests: [['this test will fail', 'fail']],
-  })
+  },
+  'handles failed tests'
+)
+
+const testFailedAsync = test('suite with failed async tests')
+
+testFailedAsync('this async test will fail', async () => {
+  return Promise.reject(Error())
 })
 
-it('reports rejected promises as failed tests', async () => {
-  const t = test('suite with failed async tests')
+const reportFailedAsync = await testFailedAsync.run()
 
-  t('this async test will fail', async () => {
-    return Promise.reject(Error())
-  })
-
-  const report = await t.run()
-
-  assert.deepStrictEqual(report, {
+assert.deepStrictEqual(
+  reportFailedAsync,
+  {
     name: 'suite with failed async tests',
     tests: [['this async test will fail', 'fail']],
-  })
-})
+  },
+  'handles failed async tests'
+)
