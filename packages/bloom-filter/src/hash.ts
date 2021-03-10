79b4e7edd86425442b0a099d0cfeae8af2ac4a07
Luis Rodrigues <luis.rodrigues@springernature.com>
2021-03-10 09:48:15 +0000

    fix: 🐛 performance improvement calculating hashes

--- packages/bloom-filter/src/hash.ts
+++ packages/bloom-filter/src/hash.ts
@@ -8,12 +8,12 @@ export function hashLocations(size: number, hashes: number, seed: number) {
   const h1 = xxh64(seed + 1)
   const h2 = xxh64(seed + 2)
 
-  const enhancedDoubleHash = (data: string, size: number, i: number) =>
-    (toUint32(h1.update(data).digest('hex')) +
-      i * toUint32(h2.update(data).digest('hex')) +
-      i ** 3) %
-    size
-
-  return (data: string) =>
-    Array.from({ length: hashes }, (_, i) => enhancedDoubleHash(data, size, i))
+  return (data: string) => {
+    const d1 = toUint32(h1.update(data).digest('hex'))
+    const d2 = toUint32(h2.update(data).digest('hex'))
+    return Array.from(
+      { length: hashes },
+      (_, i) => (d1 + i * d2 + i ** 3) % size
+    )
+  }
 }
