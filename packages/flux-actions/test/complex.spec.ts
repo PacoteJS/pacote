f97a7c28fa3168389619aad16719e60c64f9d3e0
Luis Rodrigues <luis.rodrigues@springernature.com>
2021-04-18 02:11:28 +0100

    refactor: 💡 update tests to use the new result functions

--- packages/flux-actions/test/complex.spec.ts
+++ packages/flux-actions/test/complex.spec.ts
@@ -1,4 +1,5 @@
 import { Result, fold, Ok, Err } from '@pacote/result'
+import { pipe } from '@pacote/pipe'
 import { createAction, reducerFromState } from '../src/index'
 
 test('complex payloads', () => {
@@ -14,10 +15,12 @@ test('complex payloads', () => {
   const reducer = reducerFromState<State>({ year: 1985 }).on(
     changeYear,
     (s, a) =>
-      fold(
-        (year) => ({ year }),
-        (error) => ({ ...s, error }),
-        a.payload
+      pipe(
+        a.payload,
+        fold(
+          (year) => ({ year }),
+          (error) => ({ ...s, error })
+        )
       )
   )
 
