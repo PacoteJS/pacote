c2ee63d76c0e6dfd8ccc3173bb3e56f2b72b4dae
Luis Rodrigues <luis.rodrigues@springernature.com>
2021-04-18 18:35:12 +0100

    refactor: 💡 refactor validate() function

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,17 +1,17 @@
 import { NonEmptyArray, concat, fromElements } from '@pacote/non-empty-array'
-import { Ok, Result, isErr, flatMap, mapErr } from '@pacote/result'
+import { Result, Ok, isErr, and, mapErr } from '@pacote/result'
 import { pipe } from '@pacote/pipe'
 
 export type Validation<T, E> = Result<T, NonEmptyArray<E>>
-
-const getErr = <T, E>(result: Validation<T, E>) =>
-  isErr(result) ? result.value : []
 
 export function lift<T, E>(
   check: (value: T) => Result<T, E>
 ): (value: T) => Validation<T, E> {
   return (value) => pipe(check(value), mapErr(fromElements))
 }
+
+const getErr = <T, E>(result: Validation<T, E>): readonly E[] =>
+  isErr(result) ? result.value : []
 
 export function validation<T, E>(
   ...checks: ((value: T) => Validation<T, E>)[]
@@ -22,7 +22,7 @@ export function validation<T, E>(
         pipe(
           check(value),
           mapErr((errors) => concat(getErr(previous), errors)),
-          flatMap(() => previous)
+          and(previous)
         ),
       Ok(value)
     )
