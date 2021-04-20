f9d871415164cf51e35fd0110f96adfd7378c868
Luis Rodrigues <luis.rodrigues@springernature.com>
2021-04-20 02:14:18 +0100

    refactor: 💡 break validation into functions

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,5 +1,5 @@
 import { NonEmptyArray, concat, fromElements } from '@pacote/non-empty-array'
-import { Result, Ok, isErr, and, mapErr } from '@pacote/result'
+import { Result, isErr, and, mapErr } from '@pacote/result'
 import { pipe } from '@pacote/pipe'
 
 export type Validation<T, E> = Result<T, NonEmptyArray<E>>
@@ -10,20 +10,19 @@ export function lift<T, E>(
   return (value) => pipe(check(value), mapErr(fromElements))
 }
 
-const getErr = <T, E>(result: Validation<T, E>): readonly E[] =>
+const prepend = <E>(before: readonly E[]) => (after: NonEmptyArray<E>) =>
+  concat(before, after)
+
+const getErr = <E>(result: Validation<unknown, E>) =>
   isErr(result) ? result.value : []
+
+const tupleSequence = <T, E>(results: readonly Validation<T, E>[]) =>
+  results.reduce((previous, result) =>
+    pipe(result, mapErr(prepend(getErr(previous))), and(previous))
+  )
 
 export function validation<T, E>(
   ...checks: ((value: T) => Validation<T, E>)[]
 ): (value: T) => Validation<T, E> {
-  return (value) =>
-    checks.reduce<Validation<T, E>>(
-      (previous, check) =>
-        pipe(
-          check(value),
-          mapErr((errors) => concat(getErr(previous), errors)),
-          and(previous)
-        ),
-      Ok(value)
-    )
+  return (value) => tupleSequence(checks.map((check) => check(value)))
 }
