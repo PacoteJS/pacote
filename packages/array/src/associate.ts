48a61efd8be2b591f279474f06347e8d8e7be747
Luis Rodrigues <luis.rodrigues@springernature.com>
2021-04-25 02:06:15 +0100

    fix: 🐛 return type Record<K, V> without Partial

--- packages/array/src/associate.ts
+++ packages/array/src/associate.ts
@@ -1,10 +1,10 @@
 export function associate<T, K extends keyof any, V>(
   transform: (value: T) => [K, V],
   array: readonly T[]
-): Partial<Record<K, V>> {
-  return array.reduce<Partial<Record<K, V>>>((previous, current) => {
+): Record<K, V> {
+  return array.reduce((previous, current) => {
     const [key, value] = transform(current)
     previous[key] = value
     return previous
-  }, {})
+  }, {} as Record<K, V>)
 }
