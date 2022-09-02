7b433f5a50bc9462f13db945e7a458af76eeadd2
Luis Rodrigues <lrodrigues@newstore.com>
2022-09-03 01:12:41 +0200

    fix: 🐛 Fix typing issue

--- packages/bloom-search/src/object.ts
+++ packages/bloom-search/src/object.ts
@@ -1,9 +1,12 @@
-export const keys = <O>(o: O) => Object.keys(o) as (keyof O)[]
+type AnyRecord = Record<string | number | symbol, unknown>
 
-export const entries = <O>(o: O) => Object.entries(o) as [keyof O, O[keyof O]][]
+export const keys = <O extends AnyRecord>(o: O) => Object.keys(o) as (keyof O)[]
+
+export const entries = <O extends AnyRecord>(o: O) =>
+  Object.entries(o) as [keyof O, O[keyof O]][]
 
 export function pick<
-  UnknownRecord extends Record<string | number | symbol, unknown>,
+  UnknownRecord extends AnyRecord,
   PickedKey extends keyof UnknownRecord
 >(keys: PickedKey[], obj: UnknownRecord): Pick<UnknownRecord, PickedKey> {
   return keys.reduce((picked, name) => {
