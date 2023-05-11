4f6cfd835171ef9aa00120662a4631ca3498419c
Luis Rodrigues <lrodrigues@newstore.com>
2023-05-11 11:46:39 +0200

    feat: 🎸 unique function

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -1,4 +1,5 @@
 export { associate } from './associate'
 export { range } from './range'
 export { times } from './times'
+export { unique } from './unique'
 export { windowed } from './windowed'
---
+++ packages/array/test/unique.spec.ts
@@ -0,0 +1,16 @@
+import { unique } from '../src/index'
+
+test('an empty array is returned unchanged', () => {
+  const actual = unique([])
+  expect(actual).toEqual([])
+})
+
+test('a unique array is returned unchanged', () => {
+  const actual = unique([3, 2, 1])
+  expect(actual).toEqual([3, 2, 1])
+})
+
+test('duplicated values are removed', () => {
+  const actual = unique([1, 3, 2, 1])
+  expect(actual).toEqual([1, 3, 2])
+})
