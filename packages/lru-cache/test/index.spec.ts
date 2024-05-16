59e49e8ab13b7f4df16900cc0d439b910cc063c1
Luis Rodrigues <lrodrigues@newstore.com>
2024-05-16 09:40:20 +0200

    test: 💍 clear method

--- packages/lru-cache/test/index.spec.ts
+++ packages/lru-cache/test/index.spec.ts
@@ -14,10 +14,10 @@ describe('set', () => {
 
   it('prevents eviction of used key', () => {
     const cache = new LRUCache(2)
-
     cache.set('stored', 'ok')
     cache.set('evicted', 'will be removed')
     cache.set('stored', 'updated')
+
     cache.set('stored over capacity', 'will evict LRU')
 
     expect(cache.get('stored')).toBe('updated')
@@ -25,29 +25,29 @@ describe('set', () => {
   })
 })
 
-test('get prevents eviction of used key', () => {
-  const cache = new LRUCache(2)
+describe('get', () => {
+  it('prevents eviction of used key', () => {
+    const cache = new LRUCache(2)
+    cache.set('evicted', 'will be removed')
+    cache.set('stored', 'ok')
+    cache.get('stored')
 
-  cache.set('stored', 'ok')
-  cache.set('evicted', 'will be removed')
-  cache.get('stored')
-  cache.set('stored over capacity', 'will evict LRU')
+    cache.set('stored over capacity', 'will evict LRU')
 
-  expect(cache.get('stored')).toBe('ok')
-  expect(cache.get('evicted')).toBe(undefined)
+    expect(cache.get('stored')).toBe('ok')
+    expect(cache.get('evicted')).toBe(undefined)
+  })
 })
 
 describe('size', () => {
   it('is 0 when cache is empty', () => {
     const cache = new LRUCache(1)
-
     expect(cache.size).toBe(0)
   })
 
   it('is greater than 0 when cache is populated', () => {
     const cache = new LRUCache(10)
     cache.set('test', 'ok')
-
     expect(cache.size).toBe(1)
   })
 })
@@ -73,7 +73,7 @@ test('clear', () => {
   const cache = new LRUCache(1)
   cache.set('delete', 1)
 
-  cache.delete('delete')
+  cache.clear()
 
   expect(cache.get('delete')).toBe(undefined)
 })
