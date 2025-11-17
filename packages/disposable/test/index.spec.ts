d45ef48f4526020af0e204a03d878057568bfd1d
Luis Rodrigues <lrodrigues@newstore.com>
2025-11-17 12:55:10 +0100

    test: 💍 uncomment tests

--- packages/disposable/test/index.spec.ts
+++ packages/disposable/test/index.spec.ts
@@ -1,24 +1,24 @@
-import { test } from 'vitest'
-// import { asyncDisposable, disposable } from '../src/index'
+import { test, vi, expect } from 'vitest'
+import { asyncDisposable, disposable } from '../src/index'
 
 test('creates a synchronously disposable resource', () => {
-  // const onDispose = vi.fn()
-  // const resource = { noop: () => {} }
-  // const getResource = () => disposable(resource, onDispose)
-  // ;(() => {
-  //   using resource = getResource()
-  //   resource.noop()
-  // })()
-  // expect(onDispose).toHaveBeenCalledTimes(1)
+  const onDispose = vi.fn()
+  const resource = { noop: () => {} }
+  const getResource = () => disposable(resource, onDispose)
+  ;(() => {
+    using disposableResource = getResource()
+    disposableResource.noop()
+  })()
+  expect(onDispose).toHaveBeenCalledTimes(1)
 })
 
 test('creates an asynchronously disposable resource', async () => {
-  // const onDispose = vi.fn(async () => {})
-  // const resource = { noop: () => {} }
-  // const getResource = () => asyncDisposable(resource, onDispose)
-  // ;(async () => {
-  //   await using resource = getResource()
-  //   resource.noop()
-  // })()
-  // expect(onDispose).toHaveBeenCalledTimes(1)
+  const onDispose = vi.fn(async () => {})
+  const resource = { noop: () => {} }
+  const getResource = () => asyncDisposable(resource, onDispose)
+  ;(async () => {
+    await using disposableResource = getResource()
+    disposableResource.noop()
+  })()
+  expect(onDispose).toHaveBeenCalledTimes(1)
 })
